cdd0da064936de813c41fa65ec60800521c735cd
Natalya Kosenko <sergodeeva@gmail.com>
2019-09-01 13:40:15 +0800

    Import secondary schools and contact details

--- schools/models.py
+++ schools/models.py
@@ -2,32 +2,37 @@ from django.db import models
 from django.contrib.gis.db import models as geo_models
 
 
-class Kindergarten(models.Model):
+class BaseModel(models.Model):
     name = models.CharField(max_length=100, blank=False)
     geometry = geo_models.PointField()
+    address = models.CharField(max_length=200, blank=True, null=True)
+    postal_code = models.CharField(max_length=6, blank=True, null=True)
+    phone_number = models.CharField(max_length=8, blank=True, null=True)
+    email_address = models.CharField(max_length=100, blank=True, null=True)
+    website_url = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return self.name
 
     class Meta:
-        # order of drop-down list items
+        abstract = True
+
+
+class Kindergarten(BaseModel):
+    class Meta:
+        verbose_name_plural = 'Kindergartens'
         ordering = ('name',)
 
-        # plural form in admin view
-        verbose_name_plural = 'Kindergartens'
 
-
-class PrimarySchool(models.Model):
-    name = models.CharField(max_length=100, blank=False)
-    geometry = geo_models.PointField()
+class PrimarySchool(BaseModel):
     kindergartens = models.ManyToManyField(Kindergarten, blank=True)
 
-    def __str__(self):
-        return self.name
-
     class Meta:
-        # order of drop-down list items
+        verbose_name_plural = 'Primary schools'
         ordering = ('name',)
 
-        # plural form in admin view
-        verbose_name_plural = 'Primary schools'
+
+class SecondarySchool(BaseModel):
+    class Meta:
+        verbose_name_plural = 'Secondary schools'
+        ordering = ('name',)
