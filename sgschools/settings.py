490c8870f9e703d2ec9f442e8cfcea3931694077
Natalya Kosenko <sergodeeva@gmail.com>
2019-09-09 22:10:09 +0800

    Allow hosts

--- sgschools/settings.py
+++ sgschools/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9wdoe&*&iw-lg5ps!!^lasid$+vw*!t055=3$9*6fp#u)dw7sp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
