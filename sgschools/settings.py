76c92027a64abfff2f949e5a0d297948b26ba44d
Natalya Kosenko <sergodeeva@gmail.com>
2019-09-09 23:02:18 +0800

    Rename static directory

--- sgschools/settings.py
+++ sgschools/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets/')
 STATIC_URL = '/static/'
 
 if os.name == 'nt':
